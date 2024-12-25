4a4ac3045e7ffbf6b27421da14d6140a884bcce8
CyberReaper00 <nahyanl63@gmail.com>
2024-12-25 19:08:18 +0500

    added new charting equation to make it more accurate and updated the UI

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const parseDate = d3.timeParse("%Y-%m-%d");
 // Define scales
 const x = d3.scaleTime().range([0, width]);
 const y = d3.scaleLinear()
-  .domain([0, 200])
+  .domain([0, 0])
   .range([height, 0]);
 
 // Define the line generator
@@ -28,18 +28,24 @@ d3.tsv("data.tsv").then(data => {
   data.forEach((d, i) => {
     d.date = parseDate(d.date);
     d.value = +d.value;
+    let hVal = 0;
 
     // Calculate the relative value based on the previous data point
     if (i > 0) {
-      d.relativeValue = data[i - 1].relativeValue + d.value;  // Add value to the previous relative point
+      d.relativeValue = data[i - 1].relativeValue + (data[i - 1].relativeValue * (d.value/100));  // Add value to the previous relative point
     } else {
       d.relativeValue = d.value;  // First point starts from the value itself
+    }
+    
+    if (hVal < d.relativeValue) {
+	hVal = d.relativeValue;
+	nHeight = hVal + (hVal * 0.3);
     }
   });
 
   // Set the domains for the axes
   x.domain(d3.extent(data, d => d.date));
-  y.domain([0, 200]);
+  y.domain([0, nHeight]);
 
   // Add the x-axis
   svg.append("g")
@@ -64,22 +70,7 @@ d3.tsv("data.tsv").then(data => {
   console.error("Error loading the data:", error);
 });
 
-/* minY = document.getElementById('min-value').innerText
+/*
+ minY = document.getElementById('min-value').innerText
  maxY = document.getElementById('max-value').innerText
- function open_menu() {
-	const odiv = document.getElementByClassNames('settings');
-	odiv.addEventListener('mousedown', function() {
-		div.style.display = 'block';
-	});
-}
-
-function close_menu() {
-	const cdiv = document.getElementByClassNames('menu_main');
-	cdiv.addEventListener('mousedown', function() {
-		div.style.display = 'none';
-	});
-}
-
-open_menu();
-close_menu();
 */
